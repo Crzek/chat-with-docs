c1665e7e0499d40f388616e468b5ecbe3f3c06d0
erick-tpx1 <javier1996.ec@gmail.com>
2025-05-15 01:10:24 +0200

    delete

--- src/extencions.py
+++ src/extencions.py
@@ -1,16 +1,3 @@
 from src.db.chromadb import ChromaDBManager
 
 chroma_db_manager = ChromaDBManager()
-
-
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-# from flask_jwt_extended import JWTManager
-# from flask_marshmallow import Marshmallow
-# from flask_mail import Mail
-
-# db = SQLAlchemy()
-# jwt = JWTManager()
-# migrate_instance = Migrate()
-# marshmallow = Marshmallow()
-# mail = Mail()
